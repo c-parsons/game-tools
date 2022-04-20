437a2eee89cc19897227c284cf3e74aaf544fe44
c-parsons <cparsons@google.com>
2022-04-19 22:45:28 -0400

    Add auction.py, a utility for helping gaia auction

---
+++ auction.py
@@ -0,0 +1,92 @@
+FACTIONS = ["Hads", "Terran", "Nevlas", "Geodens"]
+
+PLAYERS = {
+  "A" : [0, 5, 10, 15],
+  "B" : [0, 3, 9, 6],
+  "C" : [1, 1, 3, 0],
+  "D": [0, 3, 6, 10],
+}
+
+def validate_inputs(factions, players):
+  if len(factions) != len(players):
+    fail()
+  for k in players:
+    if len(factions) != len(players[k]):
+      fail()
+    contains_zero = False
+    for bid in players[k]:
+      if bid < 0:
+        fail()
+      if bid == 0:
+        contains_zero = True
+    if not contains_zero:
+      fail()
+  pass
+
+# If it's player_index's turn to bid, with reserve as their
+# reserve and current_bids are the current bids, this returns
+# (faction_index, bid_score) where that is the "best bid"
+# for that player.
+def best_bid(player_index, current_bids, reserve):
+  # util_val for i is the assessed value of faction i
+  # high falues are more appealing; bid on the faction
+  # with highest value.
+  # Note that best util should always be non-negative, or
+  # something went wrong.
+  best_util = -1
+  best_bid = -1
+  best_choice = -1
+  for i in range(0, len(reserve)):
+    potential_bid = 0
+    if current_bids[i] is not None:
+      (_, last_bid) = current_bids[i]
+      potential_bid = last_bid + 1
+    util_val = reserve[i] - potential_bid
+    if util_val > best_util:
+      best_util = util_val
+      best_bid = potential_bid
+      best_choice = i
+  if best_util < 0:
+    fail("Something went horribly wrong")
+  return (best_choice, best_bid)
+
+
+def bidding_algorithm(factions, players):
+  # current_bid[i] consists of (player_index, bid_score) where player_index was the last person
+  # to bid bid_score for that faction, or None if no player has yet bid on that faction.
+  current_bids = [None] * len(players)
+
+  # FIFO queue of players who need to bid. players_to_go[0] is front of queue.
+  players_to_go = []
+  players_to_go.extend(players.keys())
+  
+  while len(players_to_go) > 0:
+    (player_index, players_to_go) = (players_to_go[0], players_to_go[1:])
+    (faction_index, bid_score) = best_bid(player_index, current_bids, players[player_index])
+    print("%s bids %s on %s" % (player_index, bid_score, factions[faction_index]))
+    if current_bids[faction_index] == None:
+      current_bids[faction_index] = (player_index, bid_score)
+    else:
+      (prev_player, prev_bid) = current_bids[faction_index]
+      if bid_score <= prev_bid:
+        fail("illegal bid")
+      current_bids[faction_index] = (player_index, bid_score)
+      players_to_go.append(prev_player)
+  return current_bids
+
+
+
+def main(factions, players):
+  validate_inputs(factions, players)
+  final_result = bidding_algorithm(factions, players)
+  print("--------")
+  player_orders = {}
+  for i in range(0, len(factions)):
+    (player, bid) = final_result[i]
+    player_orders[player] = (factions[i], bid)
+  for player in players.keys():
+    (faction, bid) = player_orders[player]
+    print("Player %s will be %s at bid %s" % (player, faction, bid))
+
+main(FACTIONS, PLAYERS)
+
